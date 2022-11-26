c5cc90eba122f9fc331627f049090afb6fff4ebb
christophernguyen6089 <christopher.nguyen10@mavs.uta.edu>
2022-11-25 21:50:43 -0600

    implemented putm still yet to comment and make sense of it

--- mfs.c
+++ mfs.c
@@ -30,6 +30,7 @@
 #include <string.h>
 #include <signal.h>
 #include <sys/stat.h>
+#include <time.h>
 
 #define WHITESPACE " \t\n"      // We want to split our command line up into tokens
                                 // so we need to define what delimits our tokens.
@@ -123,6 +124,110 @@ int df()
         }
     }
     return count*BLOCK_SIZE;
+}
+
+void put(char * filename)
+{
+    struct stat buf;
+    
+    //does the file exist?
+    int status = stat(filename, &buf);
+    
+    //if the file doesnt exists, bails out of function
+    if(status==-1)
+    {
+        printf("Error: File not found.\n");
+        return;
+    }
+
+    if(buf.st_size>df( ) )
+    {
+        printf("Error: Not enough room for file.\n");
+        return;
+    }
+
+    int dir_idx = findFreeDirectoryEntry( );
+    if(dir_idx==-1)
+    {
+        printf("Error: Not enough room for file.\n");
+        return;
+    }
+
+    directory_ptr[dir_idx].valid=1;
+    directory_ptr[dir_idx].name = (char *)malloc(strlen(filename));
+    strncpy(directory_ptr[dir_idx].name,filename, strlen(filename));
+
+    int inode_idx=findFreeInode( );
+    if(inode_idx==-1)
+    {
+        printf("Error: No free inodes.\n");
+        return;
+    }
+
+    directory_ptr[dir_idx].inode_idx=inode_idx;
+    inode_array_ptr[inode_idx]->size=buf.st_size;
+    inode_array_ptr[inode_idx]->date=time(NULL);
+    inode_array_ptr[inode_idx]->valid=1;
+
+    FILE * ifp = fopen(filename, "r");
+
+    int copy_size=buf.st_size;
+    int offset=0;
+
+    int block_index=findFreeBlock( );
+    if(block_index==-1)
+    {
+        printf("Error: Can't find free node blocks.\n");
+        //TODO: Clean up a bunch of directory and inode stuff
+        //This error means that there is a bug in the code and the files are messed up somehow
+        return;
+    }
+
+    
+
+    
+
+    while(copy_size>=BLOCK_SIZE)
+    {
+        fseek(ifp, offset, SEEK_SET);
+
+        int bytes=fread(data_blocks[block_index], BLOCK_SIZE, 1, ifp);
+
+        if(bytes==0 && !feof(ifp) )
+        {
+            printf("An error occured reading from the input file.\n");
+            return;
+        }
+
+
+        used_blocks[block_index]=1;
+        //inode_array_ptr[inode_idx]->blocks[findFreeInode]
+
+        clearerr(ifp);
+        copy_size   -=BLOCK_SIZE;
+        offset      +=BLOCK_SIZE;
+    }
+    //handles the remainder
+    if(copy_size>0)
+    {
+        int block_index=findFreeBlock( );
+        if(block_index==-1)
+        {
+            printf("Error: Can't find free block.\n");
+            //cleanup a bunch of directory and inode stuff
+            return;
+        }
+
+        used_blocks[block_index]=1;
+
+        // inode_array_ptr[inode_idx]->blocks[findFreeInode()]
+
+
+        fseek(ifp, offset, SEEK_SET);
+        int bytes=fread(data_blocks[block_index], copy_size, 1, ifp);
+    }
+
+    return;
 }
 
 int main()
